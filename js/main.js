ac2d6e9b69d41f4f9ed0084c867e3faa9107f1ac
been04 <67615279+been04@users.noreply.github.com>
2020-08-26 11:26:19 +0900

    ver1.2

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function(){
+    //sub_kidari
+    $('.k_more').click(function(){
+        $('#sub').css( 'display', 'block' );
+        $('.sub_back').css( 'display', 'block' );
+        $('#sub > .kidari').css( 'display', 'block' );
+    });
+
+    //sub_star_me
+    $('.s_more').click(function(){
+        $('#sub').css( 'display', 'block' );
+        $('.sub_back').css( 'display', 'block' );
+        $('#sub > .star_me').css( 'display', 'block' );
+    });
+
+    //sub_beer
+    $('.b_more').click(function(){
+        $('#sub').css( 'display', 'block' );
+        $('.sub_back').css( 'display', 'block' );
+        $('#sub > .beer').css( 'display', 'block' );
+    });
+
+     //sub_life
+     $('.l_more').click(function(){
+        $('#sub').css( 'display', 'block' );
+        $('.sub_back').css( 'display', 'block' );
+        $('#sub > .life').css( 'display', 'block' );
+    });
+
+    //sub_love
+    $('.lv_more').click(function(){
+        $('#sub').css( 'display', 'block' );
+        $('.sub_back').css( 'display', 'block' );
+        $('#sub > .love').css( 'display', 'block' );
+    });
+
+    //sub_disney
+    $('.d_more').click(function(){
+        $('#sub').css( 'display', 'block' );
+        $('.sub_back').css( 'display', 'block' );
+        $('#sub > .disney').css( 'display', 'block' );
+    });
+
+
+
+
+    
+
+    //x
+    $('x').click(function(){
+        $('#sub').css( 'display', 'none' );
+    });
+
+})
